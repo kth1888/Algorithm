28a5a8649d327f2554ccc4faf4cb4fb49677f72c
Tae Hyoung Kang <kth1888@gmail.com>
2024-05-11 01:51:05 +0900

    Solved.ac Class 3 11727

---
+++ Class/3/11727.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+long long cases[1001] = {
+    0,
+};
+
+void solve(long long N)
+{
+    for (int i = 3; i <= N; i++)
+    {
+        cases[i] = (cases[i - 1] + 2 * cases[i - 2]) % 10007;
+    }
+
+    cout << cases[N] << '\n';
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cases[0] = 0;
+    cases[1] = 1;
+    cases[2] = 3;
+
+    long long N;
+    cin >> N;
+
+    solve(N);
+    return 0;
+}
